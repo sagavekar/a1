b55688187a2714305f27af2d7e0391dfc7d4cade
Omkar <109603669+sagavekar@users.noreply.github.com>
2023-07-15 23:25:16 +0530

    Add files via upload

---
+++ myportfolio/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from myportfolio import view
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', view.Home),
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ myportfolio/view.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+from django.shortcuts import render # to render html page
+
+def Home(request):
+    return render(request,"index.html") # rendering HTML page
