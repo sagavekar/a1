308458a36a40af4cc6b01ad4f42629052759a0f2
Omkar <109603669+sagavekar@users.noreply.github.com>
2023-07-25 17:21:11 +0530

    Update urls.py
    add studentapi path

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from myportfolio import view
+from api import views
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,7 +9,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view.Home),
-
+    path('studentapi/',views.StudentAPI.as_view() ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
