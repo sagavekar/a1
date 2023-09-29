820e67d0b545498d6832a7f5a198942cef0e320b
Omkar Sagavekar <sagavekar.om@gmail.com>
2023-09-29 18:08:41 +0530

    updated

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', view.Home),
+    path('admin', admin.site.urls),
+    path('', view.Home , name="Home"),
+    path('contact', view.contact , name="contact"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- myportfolio/view.py
+++ myportfolio/view.py
@@ -4,3 +4,6 @@ from django.shortcuts import render # to render html page
 def Home(request):
     return render(request,"index.html") # rendering HTML page
 
+def contact(request):
+    return render(request,"contact.html") # rendering HTML page
+
